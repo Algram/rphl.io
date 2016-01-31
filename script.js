7df552a1790b2a7bdb89dde701c386a2daa9906f
Algram <aliasgram@gmail.com>
2016-01-31 19:13:51 +0100

    Added command execution prototype

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
 $(document).ready(function() {
     reset();
 
+    var commands = [
+        {key: 'help', val: "I can't help u right now"}
+    ];
+
     function appendLine(str) {
         $('#terminal').append('<br><span>' + str + '</span>');
     }
 
     function reset() {
-        $('input').val('~');
+        $('input').val('');
+    }
+
+    function execute(command) {
+        for (var i = 0; i < commands.length; i++) {
+            if (commands[i].key == command) {
+                appendLine(commands[i].val);
+                reset();
+            }
+        }
     }
 
 
     $('input').on('keydown', function(e) {
         if (e.keyCode == 13) {
-            appendLine($(this).val());
-            reset();
+            var text = $(this).val();
+            var executed = false;
+
+            for (var i = 0; i < commands.length; i++) {
+                if (commands[i].key == text) {
+                    console.log('Yaas');
+                    execute(text);
+                    executed = true;
+                }
+            }
+
+            if (!executed) {
+                appendLine($(this).val());
+                reset();
+            }
         }
     });
 });
