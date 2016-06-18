0c7ea22354eb8d38d56d7647f06b0de97d993d1b
Algram <aliasgram@gmail.com>
2016-06-18 20:06:04 +0200

    Added navigation, created layout

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
-  var INPUT = $('input');
+  var TERMINAL = document.getElementById('terminal');
+  var INPUT = $('#terminal-input input');
   var INPUT_CHAR = $('#terminal-input span');
   var CONTENT = $('#terminal-content');
+
   var commands = [{
     key: 'help',
-    val: "I can't help u right now"
+    val: "I can't help you right now"
   }];
 
   reset();
+
+  $('#terminal').click(function() {
+    INPUT.focus();
+  });
 
   function reset() {
     INPUT.val('');
@@ -16,10 +22,8 @@ $(document).ready(function() {
       '<span class="teal">raphael</span> in ' +
       '<span class="yellow">~/dev/rphl.io</span>'
     );
-
-    //INPUT_CHAR.text('→');
-
     INPUT.val('');
+    TERMINAL.scrollTop = TERMINAL.scrollHeight;
   }
 
   $('input').on('keydown', function(e) {
@@ -33,51 +37,32 @@ $(document).ready(function() {
     }
   });
 
-  function clear() {
-    reset();
-    CONTENT.empty();
-  }
-
-
   function appendLine(str) {
     CONTENT.append('<li>' + '<span class="red">→</span> ' + str + '</li>');
   }
 
-
+  function appendAnswer(str) {
+    CONTENT.append('<li>' + str + '</li>');
+  }
 
   function execute(command) {
     switch (command) {
       case 'clear':
-        clear();
+        reset();
+        CONTENT.empty();
         break;
       default:
+        var found = false;
+        for (var i = 0; i < commands.length; i++) {
+          if (commands[i].key == command) {
+            found = true;
+            appendAnswer(commands[i].val);
+          }
+        }
 
-    }
-    for (var i = 0; i < commands.length; i++) {
-      if (commands[i].key == command) {
-        appendLine(commands[i].val);
-      }
+        if (!found) {
+          appendAnswer(command + ': Command not found...');
+        }
     }
   }
-
-
-  /*
-  $('input').on('keydown', function(e) {
-      if (e.keyCode == 13) {
-          var text = $(this).val();
-          var executed = false;
-
-          for (var i = 0; i < commands.length; i++) {
-              if (commands[i].key == text) {
-                  execute(text);
-                  executed = true;
-              }
-          }
-
-          if (!executed) {
-              appendLine($(this).val());
-              reset();
-          }
-      }
-  });*/
 });
